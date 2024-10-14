22621170a4f1c8ff0923340d70517a675e0865e0
Grass1o <hardboy696@gmail.com>
2024-10-14 11:17:38 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,10 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+def main_menu():
+    buttons = [
+        [KeyboardButton('Показать список ваших сотрудников')],
+        [KeyboardButton('Добавить день рождения нового сотрудника')],
+        [KeyboardButton('Как работают уведомления')],
+        [KeyboardButton('Удалить сотрудника')]
+    ]
+    return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
