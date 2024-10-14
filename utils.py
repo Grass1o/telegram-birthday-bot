af8e391b5136b0c21c34959fa84fe15ab95520b9
Grass1o <hardboy696@gmail.com>
2024-10-14 11:18:11 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,8 @@
+from datetime import datetime
 
+def validate_birthday(birthday):
+    try:
+        datetime.strptime(birthday, "%d.%m.%Y")
+        return True
+    except ValueError:
+        return False
